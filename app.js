b98c52cc6951698b6994aa293ce7679425ddfd6c
zxd <yrqc53719@163.com>
2018-12-24 18:05:33 +0800

    目录调整

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 网站页面
+var indexRouter = require('./routes/websiteRoute/index');
+// 后台管理页面
+var usersRouter = require('./routes/adminRoute/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('.html',require('ejs').__express);
+app.set('view engine', 'html');
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/web', indexRouter);
+app.use('/admin', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
