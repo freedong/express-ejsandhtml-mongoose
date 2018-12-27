39e1c128599e177361e7e9b15c5d9f77869eebc0
zxd <yrqc53719@163.com>
2018-12-28 00:33:39 +0800

    完成首页数据展示

--- controller/web/index.js
+++ controller/web/index.js
@@ -1,4 +1,8 @@
 // 控制器   控制页面跳转和渲染页面的
+var mongoose=require('mongoose');
+var News =mongoose.model('News');
+var Recruit =mongoose.model('Recruit');
+var Quit =mongoose.model('Quit');
 
 
 // 首页
@@ -7,3 +11,70 @@ exports.index = function(req,res){
     title:"Home"
   });
 };
+
+// 首页获取6条新闻
+exports.get_news = function(req,res){
+	//获取新闻处理逻辑
+	News.find({category:'0'}).sort('-_id').limit(6).exec(function(err,datas){
+		if(err){
+			console.log('获取数据失败');
+            var data={
+                status: 'failed',
+            };
+            res.json(data);
+		}else{
+			var data={
+                status: 'success',
+                datas: datas
+            };
+            res.json(data);
+		}
+	});
+};
+
+
+
+// 首页获取3条招聘
+exports.get_recruits= function(req, res) {
+    //获取招聘处理逻辑
+    Recruit.find({}).sort('-_id').limit(3).exec(function(err,datas){
+        console.log(datas);
+        if(err){
+            console.log('获取数据失败');
+            var data={
+                status: 'failed',
+            };
+            res.json(data);
+        }else{
+            var data={
+                status: 'success',
+                datas: datas
+            };
+            res.json(data);
+        }
+    });
+};
+
+
+
+// 首页获取2条离职公告
+exports.get_quits= function(req, res) {
+    //获取招聘处理逻辑
+    Quit.find({}).sort('-_id').limit(2).exec(function(err,datas){
+        if(err){
+            console.log('获取数据失败');
+            var data={
+                status: 'failed',
+            };
+            res.json(data);
+        }else{
+            var data={
+                status: 'success',
+                datas: datas
+            };
+            res.json(data);
+        }
+    });
+};
+
+
--- routes/websiteRoute/index.js
+++ routes/websiteRoute/index.js
@@ -4,4 +4,10 @@ var Index = require("../../controller/web/index");
 module.exports = function(app){
   // 首页路由
   app.get('/',Index.index);
+  // 首页获取6条新闻
+  app.get('/web/get_news', Index.get_news);
+  // 获取三条招聘信息
+  app.get('/web/get_recruits', Index.get_recruits);
+  // 获取离职公示信息2条
+  app.get('/web/get_quits', Index.get_quits);
 };
